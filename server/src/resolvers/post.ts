ba5c52012d3f864835a619749eb76eaa68170670
bastienxs <bastienxs@gmail.com>
2021-03-26 12:04:46 +0100

    fetch username from posts

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -49,21 +49,31 @@ export class PostResolver {
   ): Promise<PaginatedPosts> {
     const realLimit = Math.min(50, limit);
     const realLimitPlusOne = realLimit + 1;
-    const qb = getConnection()
-      .getRepository(Post)
-      .createQueryBuilder("p")
-      .orderBy('"createdAt"', "DESC")
-      .take(realLimitPlusOne);
 
-    if (cursor) {
-      qb.where('"createdAt" < :cursor', {
-        cursor: new Date(parseInt(cursor)),
-      });
+    const replacements: any[] = [realLimitPlusOne]
+
+    if (cursor){
+      replacements.push(new Date(parseInt(cursor)))
     }
 
-    const posts = await qb.getMany();
+    const posts = await getConnection().query(`
+    select p.*, 
+    json_build_object(
+      'id', u.id,
+      'username', u.username,
+      'email', u.email
+      ) creator 
+    from post p
+    inner join public.user u on u.id = p."creatorId"
+    ${cursor ? `where p."createdAt" < $2` : ''}
+    order by p."createdAt" DESC
+    limit $1
+    `, replacements)
 
-    return { posts: posts.slice(0, realLimit), hasMore: posts.length === realLimitPlusOne };
+    return {
+      posts: posts.slice(0, realLimit),
+      hasMore: posts.length === realLimitPlusOne,
+    };
   }
 
   @Query(() => Post, { nullable: true })
