e434d40efcda5c3e59305a6e7a540f17b16af2d8
bastienxs <bastienxs@gmail.com>
2021-03-29 15:25:50 +0200

    title and create post included into navbar

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Link } from '@chakra-ui/react';
+import { Box, Button, Flex, Heading, Link } from '@chakra-ui/react';
 import React from 'react'
 import NextLink from 'next/link'
 import { useLogoutMutation, useMeQuery } from '../generated/graphql';
@@ -9,11 +9,11 @@ interface NavBarProps {
 }
 
 export const NavBar: React.FC<NavBarProps> = ({ }) => {
-  const [{fetching : logoutFetching}, logout] = useLogoutMutation()
+  const [{ fetching: logoutFetching }, logout] = useLogoutMutation()
   const [{ data, fetching }] = useMeQuery({
-    pause : isServer(),
+    pause: isServer(),
   })
-  
+
   let body = null;
 
   //data is loading
@@ -32,22 +32,34 @@ export const NavBar: React.FC<NavBarProps> = ({ }) => {
     // user is logged in
   } else {
     body = (
-      <Flex>
+      <Flex align='center'>
+        <NextLink href='create-post'>
+          <Button as={Link} backgroundColor='whiteAlpha.600' mr={4}>
+            create post
+          </Button>
+        </NextLink>
         <Box mr={4}>{data.me.username}</Box>
-        <Button 
-        variant='link'
-        onClick={() => logout()} 
-        isLoading={logoutFetching}
-        >logout</Button>
+        <Button
+          variant='link'
+          onClick={() => logout()}
+          isLoading={logoutFetching}
+        >
+          logout
+          </Button>
       </Flex>
     )
   }
 
   return (
     <Flex position='sticky' top={0} zIndex={1} bg='tan' p={4} >
-      <Box ml={'auto'}>
-        {body}
-      </Box>
+      <Flex flex={1} maxW={800} m='auto' align='center'>
+        <NextLink href='/'>
+          <Link>
+            <Heading textColor='white'>LiReddit</Heading>
+          </Link>
+        </NextLink>
+        <Box ml={'auto'}>{body}</Box>
+      </Flex>
     </Flex>
   );
 }
