d49dc98640ceca91d471015da8f5c3a9de25b310
bastienxs <bastienxs@gmail.com>
2021-03-31 15:41:48 +0200

    session type

--- server/src/types.ts
+++ server/src/types.ts
@@ -1,13 +1,15 @@
 import { Request, Response } from "express";
-import { Session } from "express-session";
+import { Session, SessionData } from "express-session";
 import { Redis } from "ioredis";
 import { createUpdootLoader } from "./utils/createUpdootLoader";
 import { createUserLoader } from "./utils/createUserLoader";
 
 export type MyContext = {
-  req: Request & { session: Session };
-  redis : Redis,
+  req: Request & {
+    session: Session & Partial<SessionData> & { userId?: number };
+  };
+  redis: Redis;
   res: Response;
-  userLoader: ReturnType<typeof createUserLoader>,
-  updootLoader: ReturnType<typeof createUpdootLoader>,
+  userLoader: ReturnType<typeof createUserLoader>;
+  updootLoader: ReturnType<typeof createUpdootLoader>;
 };
